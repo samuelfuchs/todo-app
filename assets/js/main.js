929e12642a84659dd1ceb5d9a269a57f396b83ef
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-25 19:20:59 -0300

    added some logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,19 @@ function testFunction(event) {
     alert('The field is empty')
   } else {
     const li = document.createElement('li')
+    const div = document.createElement('div')
+    const buttonAdd = document.createElement('button')
+    const buttonDel = document.createElement('button')
+
     li.appendChild(document.createTextNode(fieldText.value))
     li.classList.add('task__list__li')
 
+    buttonAdd.classList.add('button')
+    buttonDel.classList.add('button')
+
     listUl.appendChild(li)
+    li.appendChild(div).appendChild(buttonAdd)
+    div.appendChild(buttonDel)
   }
 }
 
