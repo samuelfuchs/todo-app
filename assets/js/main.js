400f376fb6972d7f04bf6c84220d34301461084f
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-25 17:49:39 -0300

    some testing done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
 const addBtn = document.querySelector('#add--button')
-let fieldText = document.getElementById('subdomain')
+const fieldText = document.getElementById('subdomain')
+
+const listUl = document.querySelector('.task__list__ul')
 
 addBtn.addEventListener('click', testFunction)
+listUl.addEventListener('click', clickedFunction)
 
 function testFunction(event) {
   event.preventDefault()
 
   if (fieldText.value == '') {
     alert('The field is empty')
+  } else {
+    fieldText
   }
+}
+
+function generateItem() {}
+
+// =======================
+function clickedFunction() {
+  console.log('this item was clicked')
 }
