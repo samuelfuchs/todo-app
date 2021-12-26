8366a5ed119d6d85e905d52ad68d13e3659ab961
Samuel Fuchs <fuchsgerson@gmail.com>
2021-12-26 08:22:19 -0300

    new tasks now display prettier buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,16 +17,25 @@ function testFunction(event) {
     const div = document.createElement('div')
     const buttonAdd = document.createElement('button')
     const buttonDel = document.createElement('button')
+    const iAdd = document.createElement('i')
+    const iDel = document.createElement('i')
 
     li.appendChild(document.createTextNode(fieldText.value))
     li.classList.add('task__list__li')
 
-    buttonAdd.classList.add('button')
-    buttonDel.classList.add('button')
+    buttonAdd.classList.add('add__button')
+    buttonDel.classList.add('delete__button')
+
+    iAdd.classList.add('fas')
+    iAdd.classList.add('fa-check-square')
+    iDel.classList.add('fas')
+    iDel.classList.add('fa-minus-square')
 
     listUl.appendChild(li)
     li.appendChild(div).appendChild(buttonAdd)
     div.appendChild(buttonDel)
+    buttonAdd.appendChild(iAdd)
+    buttonDel.appendChild(iDel)
   }
 }
 
